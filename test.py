c981316ef71ad1a518bf13e541586e1eb5e50985
vorreakboth.chhoeurn <vorreakboth.chhoeurn@allweb.com.kh>
2020-12-30 17:44:07 +0700

    nothing change

--- test.py
+++ test.py
@@ -136,44 +136,63 @@
 # root.mainloop()
 
 
-import tkinter as tk 
+# import tkinter as tk 
 
 
-Num_Vertical = ('ehllfslf\nn\nn\nn\nn\nn\nn\nn\nn\nn\nn\nn\nn\nn\nn\nn\nn') 
-Num_Horizontal = ("A B C D E F G H I J K L M N O P Q R S T U V \  W X Y Z") 
+# Num_Vertical = ('ehllfslf\nn\nn\nn\nn\nn\nn\nn\nn\nn\nn\nn\nn\nn\nn\nn\nn') 
+# Num_Horizontal = ("A B C D E F G H I J K L M N O P Q R S T U V \  W X Y Z") 
 
-window = tk.Tk() 
-window.geometry("250x200") 
+# window = tk.Tk() 
+# window.geometry("250x200") 
 
-SVBar = tk.Scrollbar(window) 
-SVBar.pack (side = tk.RIGHT, 
-			fill = "y") 
+# SVBar = tk.Scrollbar(window) 
+# SVBar.pack (side = tk.RIGHT, 
+# 			fill = "y") 
 
-SHBar = tk.Scrollbar(window, 
-					orient = tk.HORIZONTAL) 
-SHBar.pack (side = tk.BOTTOM, 
-			fill = "x") 
+# SHBar = tk.Scrollbar(window, 
+# 					orient = tk.HORIZONTAL) 
+# SHBar.pack (side = tk.BOTTOM, 
+# 			fill = "x") 
 
-TBox = tk.Text(window, 
-			height = 500, 
-			width = 500, 
-			yscrollcommand = SVBar.set, 
-			xscrollcommand = SHBar.set, 
-			wrap = "none") 
+# TBox = tk.Text(window, 
+# 			height = 500, 
+# 			width = 500, 
+# 			yscrollcommand = SVBar.set, 
+# 			xscrollcommand = SHBar.set, 
+# 			wrap = "none") 
 
-TBox = tk.Text(window, 
-			height = 500, 
-			width = 500, 
-			yscrollcommand = SVBar.set, 
-			xscrollcommand = SHBar.set, 
-			wrap = "none") 
+# TBox = tk.Text(window, 
+# 			height = 500, 
+# 			width = 500, 
+# 			yscrollcommand = SVBar.set, 
+# 			xscrollcommand = SHBar.set, 
+# 			wrap = "none") 
 
-TBox.pack(expand = 0, fill = tk.BOTH) 
+# TBox.pack(expand = 0, fill = tk.BOTH) 
 
-TBox.insert(tk.END, Num_Horizontal) 
-TBox.insert(tk.END, Num_Vertical) 
+# TBox.insert(tk.END, Num_Horizontal) 
+# TBox.insert(tk.END, Num_Vertical) 
 
-SHBar.config(command = TBox.xview) 
-SVBar.config(command = TBox.yview) 
+# SHBar.config(command = TBox.xview) 
+# SVBar.config(command = TBox.yview) 
 
-window.mainloop() 
+# window.mainloop() 
+
+
+# import tkinter as tk
+# from tkinter import ttk
+
+# root = tk.Tk()
+
+# mytext = tk.StringVar(value='test ' * 30)
+
+# myframe = ttk.Frame(root)
+# myentry = ttk.Entry(myframe, textvariable=mytext, state='readonly')
+# myscroll = ttk.Scrollbar(myframe, orient='horizontal', command=myentry.xview)
+# myentry.config(xscrollcommand=myscroll.set)
+
+# myframe.grid()
+# myentry.grid(row=1, sticky='ew')
+# myscroll.grid(row=2, sticky='ew')
+
+# root.mainloop()
